db756c924b47b441c10d82ee9d872b19a813148e
yymin1022 <yymin1022@gmail.com>
2022-05-01 16:29:38 +0900

    Update main.py : Apply Reversed Data

--- main.py
+++ main.py
@@ -12,15 +12,27 @@ dataFile = open("Blog_DB.json", "r")
 dataJson = json.load(dataFile)
 dataFile.close()
 
-num = 1
+dataList = []
 for dataItem in dataJson:
-    doc_ref = db.collection(u"blog").document(str(num).zfill(5))
-    doc_ref.set({
+    dataList.append({
         u"isPinned": dataItem["pinned"],
         u"date": dataItem["postDate"],
         u"tag": dataItem["postTag"],
         u"title": dataItem["postTitle"],
         u"url": dataItem["postURL"],
+    })
+
+dataList.reverse()
+
+num = 1
+for dataItem in dataList:
+    doc_ref = db.collection(u"blog").document(str(num).zfill(5))
+    doc_ref.set({
+        u"isPinned": dataItem["isPinned"],
+        u"date": dataItem["date"],
+        u"tag": dataItem["tag"],
+        u"title": dataItem["title"],
+        u"url": dataItem["url"],
     })
     num += 1
 
@@ -28,15 +40,27 @@ dataFile = open("Project_DB.json", "r")
 dataJson = json.load(dataFile)
 dataFile.close()
 
-num = 1
+dataList = []
 for dataItem in dataJson:
-    doc_ref = db.collection(u"project").document(str(num).zfill(5))
-    doc_ref.set({
+    dataList.append({
         u"isPinned": dataItem["pinned"],
         u"date": dataItem["postDate"],
         u"tag": dataItem["postTag"],
         u"title": dataItem["postTitle"],
         u"url": dataItem["postURL"],
+    })
+
+dataList.reverse()
+
+num = 1
+for dataItem in dataList:
+    doc_ref = db.collection(u"project").document(str(num).zfill(5))
+    doc_ref.set({
+        u"isPinned": dataItem["isPinned"],
+        u"date": dataItem["date"],
+        u"tag": dataItem["tag"],
+        u"title": dataItem["title"],
+        u"url": dataItem["url"],
     })
     num += 1
 
@@ -44,14 +68,26 @@ dataFile = open("Solving_DB.json", "r")
 dataJson = json.load(dataFile)
 dataFile.close()
 
-num = 1
+dataList = []
 for dataItem in dataJson:
-    doc_ref = db.collection(u"solving").document(str(num).zfill(5))
-    doc_ref.set({
+    dataList.append({
         u"isPinned": dataItem["pinned"],
         u"date": dataItem["postDate"],
         u"tag": dataItem["postTag"],
         u"title": dataItem["postTitle"],
         u"url": dataItem["postURL"],
+    })
+
+dataList.reverse()
+
+num = 1
+for dataItem in dataList:
+    doc_ref = db.collection(u"solving").document(str(num).zfill(5))
+    doc_ref.set({
+        u"isPinned": dataItem["isPinned"],
+        u"date": dataItem["date"],
+        u"tag": dataItem["tag"],
+        u"title": dataItem["title"],
+        u"url": dataItem["url"],
     })
     num += 1
