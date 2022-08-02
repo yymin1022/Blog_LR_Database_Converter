2f54d7b435b463be90ce8706d943a19e28ae8407
yymin1022 <yymin1022@gmail.com>
2022-08-03 02:11:35 +0900

    Update Converting Code

--- ConvertBOJ.py
+++ ConvertBOJ.py
@@ -3,91 +3,59 @@ import json
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+from bs4 import BeautifulSoup
+from urllib import request
+from pathlib import Path
+
+import os
+import shutil
+import time
+
 cred = credentials.Certificate("blog-lr-b18ce-0de4250a02bf.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-dataFile = open("Blog_DB.json", "r")
-dataJson = json.load(dataFile)
-dataFile.close()
+postList = os.listdir("../Blog_LR_Data_Temp")
 
-dataList = []
-for dataItem in dataJson:
-    dataList.append({
-        u"isPinned": dataItem["pinned"],
-        u"date": dataItem["postDate"],
-        u"tag": dataItem["postTag"],
-        u"title": dataItem["postTitle"],
-        u"url": dataItem["postURL"],
-    })
+# Add Posting Layout on Each File
+for post in postList:
+    with open(f"../Blog_LR_Data_Temp/{post}/post.md") as f1:
+        with open(f"../Blog_LR_Data_Temp/{post}/temp.md", "w") as f2:
+            f2.write(f"[문제 바로가기](https://boj.kr/{post})\n")
+            f2.write("\n")
+            f2.write("```c++\n")
+            for line in f1:
+                f2.write(line)
+            f2.write("```")
 
-dataList.reverse()
+# Remove Temporaty Files
+for post in postList:
+    os.mkdir(f"../Blog_LR_Data_Temp/boj-{post}")
+    with open(f"../Blog_LR_Data_Temp/{post}/temp.md") as f1:
+        with open(f"../Blog_LR_Data_Temp/boj-{post}/post.md", "w") as f2:
+            for line in f1:
+                f2.write(line)
 
-num = 1
-for dataItem in dataList:
-    doc_ref = db.collection(u"blog").document(str(num).zfill(5))
-    doc_ref.set({
-        u"isPinned": dataItem["isPinned"],
-        u"date": dataItem["date"],
-        u"tag": dataItem["tag"],
-        u"title": dataItem["title"],
-        u"url": dataItem["url"],
-    })
-    num += 1
+postList = os.listdir("../Blog_LR_Data/solving")
+postList.sort()
 
-dataFile = open("Project_DB.json", "r")
-dataJson = json.load(dataFile)
-dataFile.close()
+# Upload Post Datas to Firebase
+for post in postList:
+    if post[0] == "b":
+        problem = post.replace("boj-", "")
+        print(problem)
 
-dataList = []
-for dataItem in dataJson:
-    dataList.append({
-        u"isPinned": dataItem["pinned"],
-        u"date": dataItem["postDate"],
-        u"tag": dataItem["postTag"],
-        u"title": dataItem["postTitle"],
-        u"url": dataItem["postURL"],
-    })
+        url = f"https://acmicpc.net/problem/{problem}"
+        html = request.urlopen(url).read().decode('utf8')
+        soup = BeautifulSoup(html, 'html.parser')
+        title = soup.title.string.replace('번:', '. ')
 
-dataList.reverse()
-
-num = 1
-for dataItem in dataList:
-    doc_ref = db.collection(u"project").document(str(num).zfill(5))
-    doc_ref.set({
-        u"isPinned": dataItem["isPinned"],
-        u"date": dataItem["date"],
-        u"tag": dataItem["tag"],
-        u"title": dataItem["title"],
-        u"url": dataItem["url"],
-    })
-    num += 1
-
-dataFile = open("Solving_DB.json", "r")
-dataJson = json.load(dataFile)
-dataFile.close()
-
-dataList = []
-for dataItem in dataJson:
-    dataList.append({
-        u"isPinned": dataItem["pinned"],
-        u"date": dataItem["postDate"],
-        u"tag": dataItem["postTag"],
-        u"title": dataItem["postTitle"],
-        u"url": dataItem["postURL"],
-    })
-
-dataList.reverse()
-
-num = 1
-for dataItem in dataList:
-    doc_ref = db.collection(u"solving").document(str(num).zfill(5))
-    doc_ref.set({
-        u"isPinned": dataItem["isPinned"],
-        u"date": dataItem["date"],
-        u"tag": dataItem["tag"],
-        u"title": dataItem["title"],
-        u"url": dataItem["url"],
-    })
-    num += 1
+        doc_ref = db.collection(u"solving").document(str(problem).zfill(5))
+        doc_ref.set({
+            u"isPinned": True,
+            u"date": f"BOJ {problem}",
+            u"tag": ["BOJ", "C++"],
+            u"title": title,
+            u"url": post,
+        })
